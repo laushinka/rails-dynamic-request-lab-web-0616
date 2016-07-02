f2c170224c18df1f11ae93779bc898b461df2c1e
laushinka <laurie.malau@gmail.com>
2016-07-02 16:08:22 -0400

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,12 @@
 RSpec.configure do |config|
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
+
+# Use color in STDOUT
+config.color = true
+
+# Use color not only in STDOUT but also in pagers and files
+config.tty = true
 =begin
   # These two settings work together to allow you to limit a spec run
   # to individual examples or groups you care about by tagging them with
